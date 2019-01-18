96c68aa68b29d4b76cf0234a6a4a3df4777eac4c
Simon Roehrl <simon_roehrl@web.de>
2019-01-18 21:09:59 +0100

    made example for http get request a bit more verbose

--- examples/GPRS_HTTP_GET/GPRS_HTTP_GET.ino
+++ examples/GPRS_HTTP_GET/GPRS_HTTP_GET.ino
@@ -10,66 +10,121 @@
 //you can use the AT Command(AT+IPR=9600) to set it through SerialDebug
 #define BAUDRATE  9600
 
+#define RETRY_COUNT 5
+
 GPRS gprs(PIN_TX, PIN_RX, BAUDRATE);
+
+//TODO adjust this
+const char apn[] PROGMEM = {"internet.telekom"};
 
 char responseBuffer[32];
 
 void setup(){
+  int i;
+
   Serial.begin(9600);
 
-  Serial.println(F("Start HTTP demonstration..."));
+  Serial.println(F("Start HTTP demonstration, yeah!"));
+  Serial.println(F("if something is failing you may want to debug, check sim900.h\r\n"));
 
-  while(!gprs.init()) {
+  for (i = 0; i < RETRY_COUNT; i++)
+  {
+      Serial.println(F("Initializing ..."));
+      if (gprs.init() != false)
+          break;
+
       delay(500);
-      Serial.println(F("Initializing..."));
   }
 
-  while(!gprs.isNetworkRegistered())
+  if (i < RETRY_COUNT)
+      Serial.println(F("OK"));
+  else
   {
-    delay(1000);
-    Serial.println(F("Network has not registered yet!"));
+      Serial.println(F("failed"));
+      return;
+  }
+
+  for (i = 0; i < RETRY_COUNT; i++)
+  {
+      Serial.println(F("Check if network is registered..."));
+      if (gprs.isNetworkRegistered() != false)
+          break;
+
+      delay(1000);
+  }
+
+  if (i < RETRY_COUNT)
+      Serial.println(F("OK"));
+  else
+  {
+      Serial.println(F("failed"));
+      return;
   }
 
   Serial.println(F("opening bearer ..."));
-  if (gprs.openBearer(F("internet.telekom")) != false)
+  if (gprs.openBearer((const __FlashStringHelper *)apn) != false)
   {
-      Serial.println(F("opening bearer OK"));
+      Serial.print(F("OK, ip address is: "));
+      Serial.println(gprs.getIPAddress());
+  }
+  else
+  {
+      Serial.println(F("failed"));
+      return;
   }
 
   Serial.println(F("initializing HTTP service ..."));
   if (gprs.httpInitialize() != false)
+      Serial.println(F("OK"));
+  else
   {
-      Serial.println(F("HTTP init OK"));
+      Serial.println(F("failed"));
+      return;
   }
 
-  for (int i = 0; i < 3; i++)
+  for (i = 0; i < RETRY_COUNT; i++)
   {
-      Serial.println(F("sending HTTP GET request ..."));
+      Serial.println(F("sending HTTP GET request to http://m2msupport.net/m2msupport/test.php ..."));
       if (gprs.httpSendGetRequest(F("http://m2msupport.net/m2msupport/test.php")) != -1)
-      {
-          Serial.println(F("sending HTTP GET request OK"));
           break;
-      }
+  }
+
+  if (i < RETRY_COUNT)
+      Serial.println(F("OK"));
+  else
+  {
+      Serial.println(F("failed"));
+      return;
   }
 
   Serial.println(F("fetching HTTP GET response ..."));
   if (gprs.httpReadResponseData(responseBuffer, sizeof(responseBuffer)) != false)
   {
-      Serial.println(F("fetching HTTP GET response OK, received data:"));
+      Serial.println(F("OK, received data:"));
       Serial.println(responseBuffer);
+  }
+  else
+  {
+      Serial.println(F("failed"));
+      return;
   }
 
   Serial.println(F("terminating HTTP service ..."));
   if (gprs.httpTerminate() != false)
+     Serial.println(F("OK"));
+  else
   {
-     Serial.println(F("HTTP terminate OK"));
+      Serial.println(F("failed"));
+      return;
   }
-
 
   Serial.println(F("closing bearer ..."));
   if (gprs.closeBearer() != false)
+      Serial.println(F("OK"));
+  else
   {
-      Serial.println(F("closing bearer OK"));
+      Serial.println(F("failed"));
+      return;
   }
 }
 
